217760e76c34fee3b64ea4e1aea3c9ff95505282
Tanguri <jungwontwo2@naver.com>
2024-04-24 15:14:54 +0900

    1158 요세푸스 문제

---
+++ 1158 요세푸스 문제.py
@@ -0,0 +1,14 @@
+from collections import deque
+N,K=map(int,input().split())
+queue = deque()
+for i in range(1,N+1):
+    queue.append(i)
+result=[]
+print("<",end="")
+while queue:
+    for i in range(K-1):
+        queue.append(queue.popleft())
+    print(queue.popleft(),end="")
+    if(queue):
+        print(', ',end="")
+print(">")
