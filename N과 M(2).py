aa388be6bd185f451932fa10bb9a04e221f0472e
Tanguri <jungwontwo2@naver.com>
2024-06-21 14:21:43 +0900

    N과M(2)

---
+++ N과 M(2).py
@@ -0,0 +1,22 @@
+def dfs():
+    if len(arr) == b:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(1, a+1):
+        if visited[i]:
+            continue
+        if(len(arr)>0):
+            if(arr[-1]>i):
+                continue
+        visited[i] = True
+        arr.append(i)
+        dfs()
+        arr.pop()
+        visited[i] = False
+
+
+a,b=map(int,input().split())
+arr=[]
+visited=[False]*(a+1)
+
+dfs()
